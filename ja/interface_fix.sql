b3f4adc32e8614628448f669ec66e8f1a765d36a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-05-23 18:33:55 +0900

    add ja/interface_fix.sql

---
+++ ja/interface_fix.sql
@@ -0,0 +1,5 @@
+UPDATE `entries` SET `entry_type`='インターフェース' WHERE `entry_type`='インタフェース';
+UPDATE `entries` SET `entry_type`='インターフェース' WHERE `entry_type`='インタフェイス';
+UPDATE `entries` SET `entry_type`='インターフェース' WHERE `entry_type`='インタフェ－ス';
+UPDATE `entries` SET `entry_type`='インターフェース' WHERE `entry_type`='インターフェイス';
+UPDATE `entries` SET `entry_type`='インターフェース' WHERE `entry_type`='インタ－フェイス';
